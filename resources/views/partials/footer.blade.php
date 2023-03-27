605fe41033a13258d53825745e12a82a3bc12690
AngheloRamos01 <90488811+AngheloRamos01@users.noreply.github.com>
2023-03-27 21:51:03 +0800

    Commit

---
+++ resources/views/partials/footer.blade.php
@@ -0,0 +1,12 @@
+    <footer class="text-white bg-dark">
+    <div class=" text-center">
+  <div class="card-body text-white bg-dark mb-3">
+    <p> MY HOME</p>
+    <h5 class="card-title">Special "DAW" treatment</h5>
+    <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+    <a href="#" class="btn btn-primary">Go somewhere</a>
+  </div>
+</div>
+    </footer>
+</body>
+</html>
